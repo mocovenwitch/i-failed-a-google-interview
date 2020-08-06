95fa21a1e08ab8e08e45d4b5cf21f7277f0662f9
mocovenwitch <mocovenwitch@gmail.com>
2020-08-06 21:40:00 +1000

    Add int location

--- fundamental-data-structures/array/main.go
+++ fundamental-data-structures/array/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 func main() {
 	tryArray()
+	tryArrayLocation()
 }
 
 func tryArray() {
@@ -12,4 +16,19 @@ func tryArray() {
 
 	// $go run main.go
 	// [1 2 3 0 0]
+}
+
+func tryArrayLocation() {
+	a := [3]int{1, 2, 3}
+
+	for i, element := range a {
+		fmt.Println(element, " at memory:", &a[i])
+	}
+
+	// 1  at memory: 0xc00011a000
+	// 2  at memory: 0xc00011a008
+	// 3  at memory: 0xc00011a010
+
+	// Off topic, we can see an int element occupies 8 bytes, then prove it
+	fmt.Println(unsafe.Sizeof(a[0]))
 }
