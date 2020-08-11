757af29ec2f2c92a091c1bb2c81a49a34a0e0fc6
mocovenwitch <mocovenwitch@gmail.com>
2020-08-12 07:27:00 +1000

    Update Kotlin plusone

--- fundamental-data-structures/array/questions/plusone/PlugOneTest.kt
+++ fundamental-data-structures/array/questions/plusone/PlugOneTest.kt
@@ -4,16 +4,29 @@ fun main() {
     val case1= intArrayOf(1, 2, 3)
     val case2= intArrayOf(3)
     val case3= intArrayOf(0)
+    val case4 = intArrayOf(9,8,7,6,5,4,3,2,1,0)
+    val case5 = intArrayOf(6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3)
+    val case6= intArrayOf(9,9)
+
 
     val po = PlusOne()
     val result1 = po.plusOne(case1)
     val result2 = po.plusOne(case2)
     val result3 = po.plusOne(case3)
+    val result4 = po.plusOne(case4)
+    val result5 = po.plusOne(case5)
+    val result6 = po.plusOne(case6)
 
     result1.forEach { print("$it, ") }
     println()
     result2.forEach { print("$it, ") }
     println()
     result3.forEach { print("$it, ") }
+    println()
+    result4.forEach { print("$it, ") }
+    println()
+    result5.forEach { print("$it, ") }
+    println()
+    result6.forEach { print("$it, ") }
 
 }
--- fundamental-data-structures/array/questions/plusone/PlusOne.kt
+++ fundamental-data-structures/array/questions/plusone/PlusOne.kt
@@ -6,16 +6,18 @@ class PlusOne {
 
     /**
      * O(n) + O(n) = O(n)
+     *
+     * result: failed! the question doesn't mention the constrains!!
      */
-    fun plusOne(digits: IntArray): IntArray {
+    fun plusOne_1stTry(digits: IntArray): IntArray {
         // quickly handle empty
         if (digits.isEmpty()) return IntArray(0)
 
         // start
-        var value = 0
-        var len = digits.size-1
+        var value = 0L
+        var len = digits.size - 1
         digits.forEach {
-            value += it * 10.toDouble().pow(len--).toInt()
+            value += it * 10.toDouble().pow(len--).toLong()
         }
 
         value++
@@ -23,14 +25,48 @@ class PlusOne {
         len = value.toString().length
         val result = IntArray(len)
         var counter = 0
-        var mod = 0
+        var mod = 0L
         do {
             len--
-            mod = (value % (10.toDouble().pow(len))).toInt()
+            mod = (value % (10.toDouble().pow(len))).toLong()
             result[counter++] = (value / 10.toDouble().pow(len)).toInt()
             value = mod
-        } while (mod > 0 && len >0)
+        } while (mod > 0 && len > 0)
 
+        return result
+    }
+
+    /**
+     * Accepted answer. O(n)
+     * - memory can be improved a bit, e.g. avoid to do a copy array
+     */
+    fun plusOne(digits: IntArray): IntArray {
+        // empty array
+        if (digits.isEmpty()) return IntArray(0)
+
+        // start
+        val len = digits.size
+
+        var result = IntArray(len + 1)
+        var carry = 1
+        // count backwards
+        for (i in len - 1 downTo 0) {
+            var value = digits[i] + carry
+            if (value >= 10) {
+                carry = 1
+                value %= 10
+            } else {
+                carry = 0
+            }
+            result[i+1] = value
+        }
+        // last carry
+        result[0] = carry
+
+        // trim
+        if (result[0] == 0) {
+            result = result.copyOfRange(1, result.size)
+        }
         return result
     }
 }
