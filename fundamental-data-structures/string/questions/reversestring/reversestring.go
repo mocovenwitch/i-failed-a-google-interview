9fb85e85374526f4ac3b80723935ce36cd63fe5a
mocovenwitch <mocovenwitch@gmail.com>
2020-08-28 09:24:50 +1000

    Reverse String Version 2 and 3

--- fundamental-data-structures/string/questions/reversestring/reversestring.go
+++ fundamental-data-structures/string/questions/reversestring/reversestring.go
@@ -5,16 +5,36 @@ import "fmt"
 // ReverseString is O(n)
 func ReverseString(s []byte) {
 
-	// only process when size is larger than 2
-	size := len(s)
-	r := size - 1
-	if size > 1 {
-		for l := 0; l < r; l++ {
-			t := s[l]
-			s[l] = s[r]
-			s[r] = t
-			r--
-		}
+	r := len(s) - 1
+	for l := 0; l < r; l++ {
+		t := s[l]
+		s[l] = s[r]
+		s[r] = t
+		r--
+	}
+	fmt.Println(s)
+}
+
+// ReverseString1 use Golang multi-valued assignment
+// what's the strategy underneeth? the memory is observed similar to function above, and slower.
+func ReverseString1(s []byte) {
+	r := len(s) - 1
+	for l := 0; l < r; l++ {
+		s[l], s[r] = s[r], s[l] // exchange s[l] and s[r]
+		r--
+	}
+	fmt.Println(s)
+}
+
+// ReverseString2 use XOR algorithm
+// The result is the same as the #0 approach. I was expecting it can save some memory...
+func ReverseString2(s []byte) {
+	r := len(s) - 1
+	for l := 0; l < r; l++ {
+		s[l] = s[l] ^ s[r]
+		s[r] = s[l] ^ s[r]
+		s[l] = s[l] ^ s[r]
+		r--
 	}
 	fmt.Println(s)
 }
--- fundamental-data-structures/string/questions/reversestring/reversestring_test.go
+++ fundamental-data-structures/string/questions/reversestring/reversestring_test.go
@@ -14,4 +14,17 @@ func Test(t *testing.T) {
 	case2 := []byte("Hannah")
 	fmt.Println(case2)
 	ReverseString(case2)
+
+	fmt.Println(case1)
+	ReverseString1(case1)
+
+	fmt.Println(case2)
+	ReverseString1(case2)
+
+	fmt.Println()
+	fmt.Println(case1)
+	ReverseString2(case1)
+
+	fmt.Println(case2)
+	ReverseString2(case2)
 }
